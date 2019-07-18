0a3ff88a1c69905a06044af6ace3dd3814a29862
study man <why_noneed@gmail.com>
2019-07-18 16:13:18 +0800

    add

---
+++ 8/print_tanh.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(-100, 100, 1000)
+
+y = np.tanh(x)
+
+plt.plot(x, y, label = "label", color = "red", linewidth = 2)
+plt.xlabel("abscissa")
+plt.ylabel("ordinate")
+plt.title("tanh Example")
+plt.show()
