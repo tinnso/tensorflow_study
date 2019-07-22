e6daa1a4904306a02b45d62488ca9e1c53524ee2
study man <why_noneed@gmail.com>
2019-07-22 14:55:54 +0800

    add

--- 8/print_tanh.py
+++ 8/print_tanh.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.linspace(-100, 100, 1000)
+#x = np.linspace(-100, 100, 1000)
+#y = np.tanh(x)
 
-y = np.tanh(x)
+x = np.linspace(-3.14, 3.14, 1000)
+y = np.sin(x)
 
 plt.plot(x, y, label = "label", color = "red", linewidth = 2)
 plt.xlabel("abscissa")
